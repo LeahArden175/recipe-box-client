d77b6ef945e9144d283bcfe16d98e4aa29d11c3a
Leah Arden <leaharden175@gmail.com>
2021-01-06 14:27:51 -0500

    routes in app complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 import AddRecipeRoute from './routes/AddRecipe/AddRecipeRoute';
 import EditRecipeRoute from './routes/EditRecipeRoute/EditRecipeRoute';
 import LoginRoute from './routes/LoginRoute/LoginRoute';
@@ -6,17 +7,23 @@ import NotFoundRoute from './routes/NotFoundRoute/NotFoundRoute';
 import RecipeListRoute from './routes/RecipeListRoute/RecipeListRoute';
 import RecipeRoute from './routes/RecipeRoute/RecipeRoute';
 import RegistrationRoute from './routes/ResgistrationRoute/RegistrationRoute';
+import LandingRoute from './routes/LandingRoute/LandingRoute';
 
 function App() {
   return (
     <div className="App">
-      <AddRecipeRoute />
-      <EditRecipeRoute />
-      <LoginRoute />
-      <NotFoundRoute />
-      <RecipeListRoute />
-      <RecipeRoute />
-      <RegistrationRoute />
+      <main>
+        <Switch>
+      <Route exact path='/' component={LandingRoute} />
+      <Route exact path='/add-recipe' component={AddRecipeRoute} />
+      <Route exact path='/edit-recipe' component={EditRecipeRoute} />
+      <Route exact path='/login' component={LoginRoute} />
+      <Route exact path='/recipe-list' component={RecipeListRoute} />
+      <Route exact path='/recipe/:id' component={RecipeRoute} />
+      <Route exact path='/registration' component={RegistrationRoute} />
+      <Route component={NotFoundRoute} />
+      </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/routes/LandingRoute/LandingRoute.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LandingRoute extends Component {
+
+    render() {
+        return (
+            <section>
+                <p>LandingRoute</p>
+            </section>
+        )
+    }
+}
