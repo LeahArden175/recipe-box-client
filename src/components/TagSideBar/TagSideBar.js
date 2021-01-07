0693fb5f95f1e8ef8b8c0150a57317396f50da08
Leah Arden <leaharden175@gmail.com>
2021-01-07 12:46:46 -0500

    get recipes for tags done

--- src/components/TagSideBar/TagSideBar.js
+++ src/components/TagSideBar/TagSideBar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Recipe_TagsService from '../../services/recipe_tags-service'
 
 export default class TagSideBar extends Component {
+
+    getRecipesForTags = (event) => {
+        event.preventDefault();
+        Recipe_TagsService.getRecipesForTags(this.props.id)
+        .then((res) => {
+            console.log(res)
+        })
+    }
+
+
   render() {
-    console.log(this.props);
     return (
-    <li>{this.props.tag_name}</li>
-    )
+      <li>
+        <button onClick={this.getRecipesForTags}>{this.props.tag_name}</button>
+      </li>
+    );
   }
 }
---
+++ src/services/recipe_tags-service.js
@@ -0,0 +1,20 @@
+import config from '../config'
+import TokenService from './token-services'
+
+const Recipe_TagsService = {
+
+    getRecipesForTags(recipe_id){
+        return fetch(`${config.API_ENDPOINT}/recipe_tags/recipe/${recipe_id}`, {
+            headers : {
+                authorization: `bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+        .then(res =>
+            (!res.ok)
+              ? res.json().then(e => Promise.reject(e))
+              : res.json()
+          )
+    }
+}
+
+export default Recipe_TagsService
