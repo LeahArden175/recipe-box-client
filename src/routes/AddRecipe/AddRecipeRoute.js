05363ee29fac96d61126a27fe25270e7a0e6707c
Leah Arden <leaharden175@gmail.com>
2021-01-15 17:35:47 -0500

    add recipe styling complete

--- src/routes/AddRecipe/AddRecipeRoute.js
+++ src/routes/AddRecipe/AddRecipeRoute.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AddRecipe from '../../components/AddRecipe/AddRecipe';
+import './AddRecipeRoute.css'
 
 export default class AddRecipeRoute extends Component {
 
     render() {
        const history = this.props.history
         return (
-            <section>
+            <section className='add-recipe'>
                 <AddRecipe history={history}/>
             </section>
         )
