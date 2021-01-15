79753b639f9bd68a3a6e93293631b82b91921297
Leah Arden <leaharden175@gmail.com>
2021-01-15 17:40:03 -0500

    not found route styling done;

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./NotFoundRoute.css";
 
 export default class NotFoundRoute extends Component {
-
-    render() {
-        return (
-            <section>
-                <p>NotFoundRoute</p>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section className="not-found">
+        <h2>404 - Page not found</h2>
+        <p>Try going back to your previous page.</p>
+      </section>
+    );
+  }
 }
